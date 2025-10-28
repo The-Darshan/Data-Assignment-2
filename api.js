17f6dcc24f32f62fe494966088c48f238406f040
Darshan Begani <140410584+The-Darshan@users.noreply.github.com>
2025-10-28 12:37:02 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-module.exports = {
+{
     "api1": "https://api.coinlore.net/api/tickers/?start=200&limit=20",
     "api2": "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
 };
